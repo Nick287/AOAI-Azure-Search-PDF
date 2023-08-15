eb5cbcedfe6fa3f3ae2ab6bab821a65ce039e9c3
Bo Wang <bwa@microsoft.com>
2023-08-15 05:57:45 +0000

    test

--- function/AzureVectorSearch.py
+++ function/AzureVectorSearch.py
@@ -34,10 +34,10 @@ class AzureVectorSearch:
     def __init__(self):  
         self.credential = AzureKeyCredential(AZURE_SEARCH_API_KEY)
 
-    def openAI_ChatCompletion(self, query):
+    def openAI_ChatCompletion(self, sys_meg, query):
         # https://platform.openai.com/docs/guides/gpt/chat-completions-api
         # retrieval = openai.ChatCompletion.create(engine=CHAT_MODEL,messages=[{'role':"user",'content': query}],max_tokens=500)
-        retrieval = openai.ChatCompletion.create(engine=CHAT_MODEL,messages=[{'role':"user",'content': query}], temperature=0.7)
+        retrieval = openai.ChatCompletion.create(engine=CHAT_MODEL,messages=[{'role':"system",'content': sys_meg},{'role':"user",'content': query}], temperature=0)
         # Response provided by GPT-3.5
         return retrieval['choices'][0]['message']['content']
 
