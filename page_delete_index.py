bb734ef884cc1ae78c2ed65de28e7523473a3d81
Bo Wang <bwa@microsoft.com>
2023-10-28 17:56:28 +0800

    update

--- page_delete_index.py
+++ page_delete_index.py
@@ -11,25 +11,18 @@ st.subheader('Delete Index')
 # azureVectorSearch = AzureVectorSearch()
 chromaHelper = ChromaHelper()
 
-if 'rerun' not in st.session_state:
-    st.session_state['rerun'] = False
-
 with st.spinner(text="Loading..."):
     # index_names = azureVectorSearch.list_index_names()
     index_names = chromaHelper.list_index_names()
-    selected_index_name = st.selectbox('Please select an index name.',index_names)
+    st.session_state.item = None
+    selected_index_name = st.selectbox('Please select an index name.',index_names,index=st.session_state.item)
 
 if st.button('Delete Index'):
     if selected_index_name == None:
         st.stop()
     else:
         with st.spinner('Deleting index...'):
-            if st.session_state.rerun:
-                st.session_state.rerun = False
-                st.stop()
-                pass
-
             # azureVectorSearch.delete_search_index(selected_index_name)
             chromaHelper.delete_index(selected_index_name)
-            st.session_state['rerun'] = True
+            st.session_state.item = None
             st.rerun()
